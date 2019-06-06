4920147209189fe43f58decaf08ab0244147465c
Jo Vliegen <jo.vliegen@esat.kuleuven.be>
2019-06-06 10:37:11 +0200

    rename sourcefile

--- OS/source/chap1/main_blinking.c
+++ OS/source/chap1/main_blinking.c
@@ -5,25 +5,16 @@ volatile unsigned int *ADDRESS_PORT_B = (volatile unsigned int*) 0x0000025;
 void setup()
 {
   DDRB |= (1 << 5);
-  // or in a shorter form:  DDRD |= PORTD6;
-  // or even with helper.h: bit_set(DDRD, PORTD6);
 }
 
 void loop()
 {
   int i, j;
 
-
-
-
-
-
   *ADDRESS_PORT_B = *ADDRESS_PORT_B | (1 << 5);
-  //PORTB = PORTB | (1 << 5);
   for(j=0;j<1600;j++) for(i=0;i<1600;i++) asm("nop");
 
   *ADDRESS_PORT_B = *ADDRESS_PORT_B & ~(1 << 5);
-  //PORTB = PORTB & ~(1 << 5);
   for(j=0;j<1600;j++) for(i=0;i<1600;i++) asm("nop");
 }
 

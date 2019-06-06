aa3bd004bb3cf0f06e6ad6555e26e6a36df45034
Jo Vliegen <jo.vliegen@esat.kuleuven.be>
2019-06-06 11:13:00 +0200

    make DDR as address

--- OS/source/chap1/main_blinking.c
+++ OS/source/chap1/main_blinking.c
@@ -1,10 +1,13 @@
 #include <avr/io.h>
 
+volatile unsigned int *ADDRESS_DDR_B = (volatile unsigned int*) 0x0000024;
 volatile unsigned int *ADDRESS_PORT_B = (volatile unsigned int*) 0x0000025;
 
 void setup()
 {
-  DDRB |= (1 << 5);
+  *ADDRESS_DDR_B |= (1 << 5);
+  // or in a shorter form:  DDRD |= PORTD6;
+  // or even with helper.h: bit_set(DDRD, PORTD6);
 }
 
 void loop()
@@ -12,9 +15,11 @@ void loop()
   int i, j;
 
   *ADDRESS_PORT_B = *ADDRESS_PORT_B | (1 << 5);
+  //PORTB = PORTB | (1 << 5);
   for(j=0;j<1600;j++) for(i=0;i<1600;i++) asm("nop");
 
   *ADDRESS_PORT_B = *ADDRESS_PORT_B & ~(1 << 5);
+  //PORTB = PORTB & ~(1 << 5);
   for(j=0;j<1600;j++) for(i=0;i<1600;i++) asm("nop");
 }
 

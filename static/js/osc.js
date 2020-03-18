5b69ed64bff1eeca15bfd4dd548e65656e6d69b5
Jo Vliegen <jo.vliegen@esat.kuleuven.be>
2020-03-18 09:55:50 +0100

    adding content to ch7

--- static/js/osc.js
+++ static/js/osc.js
@@ -1,5 +1,38 @@
-function showAnswer(target) {
+function toggleAnswer(target) {
+  if(target ==='q711') {
+    x = document.getElementById('q711')
+    if (x.style.visibility === "hidden") {
+      x.style.visibility = "visible";
+    } else {
+      x.style.visibility = "hidden";
+    }
+  }
+
+  if(target ==='q712') {
+    x = document.getElementById('q712')
+    if (x.style.visibility === "hidden") {
+      x.style.visibility = "visible";
+    } else {
+      x.style.visibility = "hidden";
+    }
+  }
+
   if(target ==='q713') {
-    document.getElementById('q713').src='../../img/sched/ss_preemt_priority_answer.png'
+    x = document.getElementById('q713')
+    if (x.src.includes('img/sched/ss_preemt_priority_answer.png')) {
+      x.src = '../../img/sched/ss_preemt_priority.png'
+    } else {
+      x.src = '../../img/sched/ss_preemt_priority_answer.png'
+    }
+    // document.getElementById('q713').src='../../img/sched/ss_preemt_priority_answer.png'
+  }
+
+  if(target ==='q714') {
+    x = document.getElementById('q714')
+    if (x.style.visibility === "hidden") {
+      x.style.visibility = "visible";
+    } else {
+      x.style.visibility = "hidden";
+    }
   }
 }  

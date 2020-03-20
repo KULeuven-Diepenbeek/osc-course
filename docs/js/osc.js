939167f60459f6f7d313a52ffe9362bca07ed309
Jo Vliegen <jo.vliegen@esat.kuleuven.be>
2020-03-20 09:05:30 +0100

    publish ch7

---
+++ docs/js/osc.js
@@ -0,0 +1,68 @@
+function toggleAnswer(target) {
+  if(target ==='q711') {
+    x = document.getElementById('q711')
+    if (x.style.visibility === "hidden") {
+      x.style.visibility = "visible";
+    } else {
+      x.style.visibility = "hidden";
+    }
+  }
+
+  if(target ==='q712') {
+    x = document.getElementById('q712')
+    if (x.style.visibility === "hidden") {
+      x.style.visibility = "visible";
+    } else {
+      x.style.visibility = "hidden";
+    }
+  }
+
+  if(target ==='q713') {
+    x = document.getElementById('q713')
+    if (x.src.includes('img/sched/ss_preemt_priority_answer.png')) {
+      x.src = '../../img/sched/ss_preemt_priority.png'
+    } else {
+      x.src = '../../img/sched/ss_preemt_priority_answer.png'
+    }
+    // document.getElementById('q713').src='../../img/sched/ss_preemt_priority_answer.png'
+  }
+
+  if(target ==='q714') {
+    x = document.getElementById('q714')
+    if (x.style.visibility === "hidden") {
+      x.style.visibility = "visible";
+    } else {
+      x.style.visibility = "hidden";
+    }
+  }
+
+  if(target ==='q721') {
+    x = document.getElementById('div_q721')
+    if (x.style.visibility === "hidden") {
+      x.style.visibility = "visible";
+    } else {
+      x.style.visibility = "hidden";
+    }
+    x = document.getElementById('img_q721')
+    if (x.src.includes('img/sched/ss_ts.png')) {
+      x.src = '../../img/sched/ss_ts10ms.png'
+    } else {
+      x.src = '../../img/sched/ss_ts.png'
+    }
+  }
+
+  if(target ==='q722') {
+    x = document.getElementById('div_q722')
+    if (x.style.visibility === "hidden") {
+      x.style.visibility = "visible";
+    } else {
+      x.style.visibility = "hidden";
+    }
+    x = document.getElementById('img_q722')
+    if (x.src.includes('img/sched/ss_ts_l.png')) {
+      x.src = '../../img/sched/ss_ts100ms.png'
+    } else {
+      x.src = '../../img/sched/ss_ts_l.png'
+    }
+  }
+}  
